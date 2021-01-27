6b072e967a0b76225919c7b8ab3d3fee707fd99e
Jonathan Lopez <jonatlop@gmail.com>
2021-01-26 19:11:10 -0500

    The navigation bar is designed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 
+import "./stylesheets/main.scss";
+
 function App() {
    return (
      <Router>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,51 @@
-export interface HeaderProps {
+import { Link } from "react-router-dom";
+
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+interface HeaderProps {
     
 }
  
 const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
     return ( 
         <nav className="header">
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
-            
+            <Link to="/">
+                <img 
+                    className="mainLogo" 
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                    alt=""/>
+            </Link>
+            <div className="header_search">
+                <input type="text" className="searchInput"/>
+                <SearchIcon className="searchIcon"/>
+            </div>
+            <div className="navBar">
+                <Link className="headerLink" to="/login">
+                    <div className="headerLink_option">
+                        <span>Hello Jonathan</span>
+                        <span>Sign in</span>
+                    </div>
+                </Link>
+                <Link className="headerLink" to="">
+                    <div className="headerLink_option">
+                        <span>Return</span>
+                        <span>& Orders</span>
+                    </div>
+                </Link>
+                <Link className="headerLink" to="">
+                    <div className="headerLink_option">
+                        <span>Your</span>
+                        <span>Prime</span>
+                    </div>
+                </Link>
+                <Link className="headerLink" to="/checkout">
+                    <div className="headerLink_optionBasket">
+                        <ShoppingBasketIcon />
+                        <span>0</span>
+                    </div>
+                </Link>
+            </div>
         </nav>
      );
 }
